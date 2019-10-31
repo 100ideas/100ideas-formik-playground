660188df69534e58f649534fca27be6e935c19b5
100ideas <macowell@gmail.com>
2019-10-31 10:43:26 -0700

    shuffleArray; typography looks good

--- index.js
+++ index.js
@@ -14,18 +14,37 @@ import { FastFieldDIY } from './components/FastFieldDIY'
 import { FormikWithDraft } from './components/DraftEditorFormik'
 import { MoreResources } from './docs/MoreResources'
 
+function pickRandomUpTo(upperBound) {
+  return Math.floor(Math.random() * Math.floor(upperBound));
+}
+
+function shuffleArray(arr){
+  let shuffled = []
+  let safety = 0;
+  while (shuffled.length < arr.length){
+    let pick = pickRandomUpTo(arr.length)
+    if(shuffled.indexOf(pick) === -1) shuffled.push(pick)
+    if(++safety > 100 * arr.length) break
+    continue
+  }
+  let shuffler = (acc, cur) => [ ...acc, arr[cur] ]
+  if(safety > 10 * arr.length) console.warn('shuffleArray iterated > 10x input length!', safety)
+  return shuffled.reduce(shuffler, [])
+}
+
+let hands = shuffleArray(['👇', '👇🏿', '👇🏼👇🏼', '👇🏽👇🏽', '👇🏾👇🏾', '👇🏿👇🏿'])
 
 const Intro = () => <section>
   <h2><em>@100ideas'</em> formik v2.0.3 playground</h2>
   <p>fork/edit this on stackblitz: <a href="https://stackblitz.com/edit/100ideas-formik-playground" >stackblitz.com/edit/100ideas-formik-playground</a></p>
   <ol>
-    <li><code>./components/TutorialPlayground</code> <a href="#SignupFormPlay">played around with offical tutorial</a></li>
+    <li><code>./components/TutorialPlayground</code> playing around with offical tutorial <a href="#SignupFormPlay">{hands[0]}</a></li>
 
-    <li><code>./components/FormikTutorialFinal.js</code><a href="#FormikTutorialFinal">Formik Tutorial Final Result</a></li>
+    <li><code>./components/FormikTutorialFinal.js</code>Formik Tutorial Final Result <a href="#FormikTutorialFinal">{hands[1]}</a></li>
 
-    <li><code>./components/FastFieldDIY.js</code> <a href="#FastInputField">FastField fix - demo of FastInputField rendering isolation</a></li>
+    <li><code>./components/FastFieldDIY.js</code>FastField fix - demo of FastInputField rendering isolation<a href="#FastInputField">{hands[2]}</a></li>
 
-    <li><code>./components/DraftEditorFormik.js</code> <a href="#formik-draftjs">how to hook Draft.js into formik for use as a custom field</a></li>
+    <li><code>./components/DraftEditorFormik.js</code>how to hook Draft.js into formik for use as a custom field<a href="#formik-draftjs">{hands[3]}</a></li>
   </ol>
   <hr />
 </section>
